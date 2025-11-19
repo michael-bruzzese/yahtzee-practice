8c1df2083f3d7cce402da45aa7921981138168fe
Michael Bruzzese <michael.bruzzese@yahoo.com>
2025-11-18 16:12:47 -0800

    chore: pin turbopack root to project

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Pin the Turbopack root to this project to avoid multi-lockfile warnings.
+  turbopack: {
+    root: __dirname,
+  },
 };
 
 export default nextConfig;
